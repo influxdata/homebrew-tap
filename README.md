1216d8129d7c4ff3f09949779ca9b2eae6cb60ba
Josh Powers <powersj@fastmail.com>
2023-10-18 08:40:54 -0600

    feat: Release influxctl v2.0.4

---
+++ README.md
@@ -0,0 +1,28 @@
+# homebrew-tap
+
+Homebrew formulae that allows installation of InfluxData tools through the
+Homebrew package manager.
+
+Please see the [InfluxData downloads][] for additional downloads that are
+available as Homebrew cores packages like including `influxdb`, `telegraf`,
+and more.
+
+[InfluxData downloads]: https://portal.influxdata.com/downloads/
+
+## Installation
+
+```s
+brew tap influxdata/tap
+brew install <formula>
+```
+
+## Formulae
+
+* `influxctl` - CLI to manage InfluxDB v3 instances
+
+## Documentation
+
+Please see the `brew help` or the [Homebrew docs][] for details on using `brew`
+along with taps.
+
+[Homebrew docs]: https://docs.brew.sh/
---
+++ influxctl.rb
@@ -0,0 +1,23 @@
+class influxctl < Formula
+    desc "CLI to manage InfluxDB v3 instances"
+    homepage "https://docs.influxdata.com/influxdb/cloud-dedicated/reference/cli/influxctl/"
+    version "2.0.4"
+
+    if Hardware::CPU.arm?
+        url "https://dl.influxdata.com/influxctl/releases/influxctl-v2.0.4-darwin-arm64.zip"
+        sha256 "2809e27f7597470334b5ae43a365c8b44b9ccd70c91a0a40aed910e5b7a34c4f"
+    end
+
+    if Hardware::CPU.intel?
+        url "https://dl.influxdata.com/influxctl/releases/influxctl-v2.0.4-darwin-x86_64.zip"
+        sha256 "d69c04d168813cb0357e15e75656924d6460295dc8174e1d7449bbe92704eeea"
+    end
+
+    def install
+        bin.install "influxctl"
+    end
+
+    test do
+        system "#{bin}/influxctl", "version"
+    end
+end
