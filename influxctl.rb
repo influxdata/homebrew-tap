5e412923f88d0f745a7b7d750807a4db2a9ac87d
Josh Powers <powersj@fastmail.com>
2024-06-26 09:34:35 -0600

    influxctl v2.9.3

--- influxctl.rb
+++ influxctl.rb
@@ -1,16 +1,16 @@
 class Influxctl < Formula
 	desc "CLI to manage InfluxDB v3 instances"
 	homepage "https://docs.influxdata.com/influxdb/cloud-dedicated/reference/cli/influxctl/"
-	version "v2.9.2"
+	version "v2.9.3"
 
 	if Hardware::CPU.arm?
-		url "https://dl.influxdata.com/influxctl/releases/influxctl-v2.9.2-darwin-arm64.zip"
-		sha256 "c23c17ff16b5b0dcc13c931037588772a1bbc2cccf428a38b17a454f1eaa8204"
+		url "https://dl.influxdata.com/influxctl/releases/influxctl-v2.9.3-darwin-arm64.zip"
+		sha256 "ab6b56392482b36f9af5af19d8eb703039ff78832e5a5f31e91c5c629188517a"
 	end
 
 	if Hardware::CPU.intel?
-		url "https://dl.influxdata.com/influxctl/releases/influxctl-v2.9.2-darwin-x86_64.zip"
-		sha256 "bd6c121b750c68e54b486df156cb41d58afbb6dceab756b1078d672a20511021"
+		url "https://dl.influxdata.com/influxctl/releases/influxctl-v2.9.3-darwin-x86_64.zip"
+		sha256 "9491c14421da5662fdc23b85660f21609f81c3282ea226d42546170473012be6"
 	end
 
 	def install
